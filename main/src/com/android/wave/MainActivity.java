16f69e8cc13732588ea0f8215e87e3d97129bc24
owen19891030 <460489105@qq.com>
2014-11-18 21:38:12 +0800

    add file

---
+++ main/src/com/android/wave/MainActivity.java
@@ -0,0 +1,37 @@
+package com.android.wave;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+
+public class MainActivity extends Activity {
+
+	private SeekBar seekBar;
+	private WaveView waveView;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		seekBar = (SeekBar) findViewById(R.id.seek_bar);
+		waveView = (WaveView) findViewById(R.id.wave_view);
+
+		seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				waveView.setProgress(progress);
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+
+			}
+		});
+	}
+}
